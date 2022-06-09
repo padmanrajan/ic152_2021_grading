b2dc8d79e0bb1118455668ba4d67dcdc01c02355
Paddy <paddy@kestrel.iitmandi.ac.in>
2022-06-09 12:48:27 +0530

    modified:   masterGrading_ic152_btech2021.py
    Added some debug statements.
    The main change is that now an explicit index is used. Now this
    forces the main dataframe (foo) to consider data for all students.
    
    Hopefully this handles all the missing data.

--- masterGrading_ic152_btech2021.py
+++ masterGrading_ic152_btech2021.py
@@ -78,34 +78,65 @@ pt1_3 = pd.concat([pt1_3,pt1_3a])
 pt1_3 = pd.concat([pt1_3,pt1_3b])
 
 # merge into single df
-foo = pd.DataFrame()
+#foo = pd.DataFrame()
+foo = pd.read_csv('allResults/ic152_dec2021_emails.csv',header=None,index_col=0)
 foo['Name'] =  pt1_1['Candidate Name']
-foo['pt1'] =  (pt1_1['Score1'] + pt1_2['Score1'] + pt1_3['Score1'] + pt1_4['Score1'])/4 # made out of 100
-foo['pt2'] =  (pt2_1['Score1'] + pt2_2['Score1'])/2 # made out of 100
-foo['pt3'] =  (pt3_1['Score1'] + pt3_2['Score1'])/2 # made out of 100
+print(foo.shape)
+#foo['pt1'] =  (pt1_1['Score1'] + pt1_2['Score1'] + pt1_3['Score1'] + pt1_4['Score1'])/4 # made out of 100
+
+foo['pt1'] =  pt1_1['Score1'] + pt1_2['Score1'] 
+foo.fillna(0,inplace=True)
+print('DEBUG1: ' + str(foo.shape))
+foo['pt1'] =  foo['pt1'] + pt1_3['Score1']
+foo.fillna(0,inplace=True)
+print('DEBUG2: ' + str(foo.shape))
+foo['pt1'] =  foo['pt1'] + pt1_4['Score1']
+foo.fillna(0,inplace=True)
+print('DEBUG3: ' + str(foo.shape))
+foo['pt1'] =  foo['pt1']/4
+print('DEBUG4: ' + str(foo.shape))
+
+
+foo['pt2'] =  pt2_1['Score1'] + pt2_2['Score1'] # made out of 100
+foo.fillna(0,inplace=True)
+foo['pt2'] =  foo['pt2']/2
+print('DEBUG5: ' + str(foo.shape))
+
+foo['pt3'] =  pt3_1['Score1'] + pt3_2['Score1'] # made out of 100
+foo.fillna(0,inplace=True)
+foo['pt3'] =  foo['pt3']/2
+print('DEBUG6: ' + str(foo.shape))
 
 # the following line takes the top 2 prog test scores and gives it 20% weightage
 foo['top2pt'] = (foo['pt1'] + foo['pt2'] + foo['pt3']-foo[['pt1','pt2','pt3']].min(axis=1))/10 # out of 20
+foo.fillna(0,inplace=True)
+print('DEBUG7: ' + str(foo.shape))
 
 # two short quizzes of 5% weightage each
 foo['sq1'] =  sq1['Grade'] # already out of 5
 foo['sq2'] =  (sq2['Grade'])/2 # out of 5
+print('DEBUG8: ' + str(foo.shape))
 
 # the re-exam of 60% weightage
 foo['reEx'] = re_all['Grade']*6
+foo.fillna(0,inplace=True)
+print('DEBUG9: ' + str(foo.shape))
 
 # total without lab component, out of 90 marks
 #foo['total90'] = foo[['top2pt','sq1','sq2','reEx']].sum(axis=1)
 
 # lab marks
 foo['lab'] = lab['Grade']
+print('DEBUG10: ' + str(foo.shape))
 
 # total 
 foo['total'] = foo[['top2pt','sq1','sq2','reEx','lab']].sum(axis=1)
+foo.fillna(0,inplace=True)
+print('DEBUG11: ' + str(foo.shape))
 
 # write required columns into csv
-foo.to_csv('allData.csv')
-foo.to_csv('ic152_2021.csv', columns=['Name','top2pt','sq1','sq2','reEx','lab','total'], header=['Name','top2pt(20)','sq1(5)','sq2(5)','reEx(60)','lab(10)','total'])
+foo.to_csv('allData_2.csv')
+foo.to_csv('ic152_2021_2.csv', columns=['Name','top2pt','sq1','sq2','reEx','lab','total'], header=['Name','top2pt(20)','sq1(5)','sq2(5)','reEx(60)','lab(10)','total'])
 
 
 
